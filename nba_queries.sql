0652e7ecceac551e800b146a666e2b0aab41dc85
bynist <bayanalbunayan@gmail.com>
2019-12-11 23:13:51 +0300

    SQL solution+Bounce  w4d4

--- nba_queries.sql
+++ nba_queries.sql
@@ -20,7 +20,7 @@
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
 
-select name, points/games from players order by points/games DESC LIMIT 20  ;
+select name, points/games as points_per_game  from players order by points/games DESC LIMIT 20  ;
 
 -- The average age for all players.
 
@@ -48,13 +48,22 @@ where games>40 ;
 
 -- The team and total points scored from all players on that team (team points)
 -- ordered from most team points to least.
-
-
-
+SELECT sum(points) as total_points 
+FROM players
+GROUP BY team
+order by total_points DESC;
 -- The age and the average points per game for that age, ordered from oldest to
 -- youngest for all ages.
-
+select age , AVG(points/games) as avg_points
+from players 
+GROUP BY age
+order by age DESC; 
 
 
 -- The team and the the number of players who score above 12 points per game on
 -- that team, ordered from the most number of players to the least number of players.
+select team, count(name) as players_num 
+from players 
+where points/games > 12
+group by team
+ order by players_num DESC;
