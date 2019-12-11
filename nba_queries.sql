13b1a5fbdd058b1e9b5cfacbb01a2e359528bfa3
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2019-12-11 22:39:45 +0300

    done :)

--- nba_queries.sql
+++ nba_queries.sql
@@ -4,34 +4,35 @@
 --------------------------------------------
 
 -- All columns for all players from the New York Knicks (NYK).
-
+select * from players where team LIKE 'NYK';
 
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
-
+ select * from players where team = 'IND' AND age < 26;
 
 
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
+select * from players ORDER BY points ASC ;
 
 
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
-
+ select name, points/game from players order by points/game DESC limit 20;
 
 
 -- The average age for all players.
-
+select avg(age) from players;
 
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
-
+select avg(age) from players where team = 'OKC';
 
 
 -- The average age for all players who played more than 40 games.
-
+select avg(age) from players where game > 40;
 
 
 
@@ -43,7 +44,7 @@
 -- The team and total points scored from all players on that team (team points)
 -- ordered from most team points to least.
 
-
+select team,points from players ORDER BY points DESC;
 
 -- The age and the average points per game for that age, ordered from oldest to
 -- youngest for all ages.
--- nba_seed.sql
+++ nba_seed.sql
@@ -1,3 +1,4 @@
+
 ----------------------------------
 -- Part 1
 
@@ -11,15 +12,15 @@ CREATE DATABASE nba_db;
 \c nba_db;
 
 -- Create table here
-
+create table players (name text , age int, team text, game int, points int);
 
 -- Insert player here
 
-
+Insert into players (name , age , team , game , points) values ('Anderson Varejao', 29, 'CLE', 25, 271);
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
-
+/c \copy players(name , age , team , game , points) FROM 'C:\Users\mahbob\Desktop\sei\homeworks\hw-week04day04-sql\nba_season_2011-2012.csv' WITH (FORMAT csv, HEADER true)
 
 --COPY players
   --(name, age, team, games, points)
