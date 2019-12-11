ce310e9b60bca9dcdc62122151c71b68a5527c37
monano <mona.gaihab@gmail.com>
2019-12-11 19:20:27 +0300

    home work solved

--- nba_queries.sql
+++ nba_queries.sql
@@ -1,54 +1,56 @@
 --------------------------------------------
 -- Part 3 - Basic SQL Queries
--- write queries to select the following
+-- write queries to SELECT the following
 --------------------------------------------
 
--- All columns for all players from the New York Knicks (NYK).
+-- All columns for all players FROM the New York Knicks (NYK).
+SELECT * FROM players WHERE team = 'NYK';
 
 
-
--- All columns for all players from the Indiana Packers (IND) who are under 26
+-- All columns for all players FROM the Indiana Packers (IND) who are under 26
 -- years old.
+SELECT * FROM players WHERE team = 'IND' AND age < 26;
 
 
-
--- All columns for all players, ordered from least points scored to most points
+-- All columns for all players, ordered FROM least points scored to most points
 -- scored.
+SELECT * FROM players ORDER BY points;
 
 
-
--- Name and Points per game (points/games), for the players with the top 20 points
+-- Name AND Points per game (points/games), for the players with the top 20 points
 -- per game.
-
+SELECT name, points/games as points_per_game  FROM players ORDER BY points DESC LIMIT 20;
 
 
 -- The average age for all players.
-
+SELECT AVG(age) FROM players;
 
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
-
+SELECT AVG(age) FROM players WHERE team = 'OKC';
 
 
 -- The average age for all players who played more than 40 games.
-
+SELECT AVG(age) FROM players WHERE games > 40;
 
 
 
 --------------------------------------------
 -- BONUS - Advanced SQL Queries
--- write queries to select the following
+-- write queries to SELECT the following
 --------------------------------------------
 
--- The team and total points scored from all players on that team (team points)
--- ordered from most team points to least.
+-- The team AND total points scored FROM all players on that team (team points)
+-- ordered FROM most team points to least.
+SELECT team, SUM(points) as total_points FROM players group by team ORDER BY total_points DESC;
 
 
 
--- The age and the average points per game for that age, ordered from oldest to
+-- The age AND the average points per game for that age, ordered FROM oldest to
 -- youngest for all ages.
+SELECT age, AVG(points/games) FROM players group by age ORDER BY age DESC;
 
 
-
--- The team and the the number of players who score above 12 points per game on
--- that team, ordered from the most number of players to the least number of players.
+-- The team AND the the number of players who score above 12 points per game on
+-- that team, ordered FROM the most number of players to the least number of players.
+SELECT team, COUNT(name) as number_of_players FROM players WHERE (points/games)>12 group by team ORDER BY number_of_players DESC;
--- nba_seed.sql
+++ nba_seed.sql
@@ -2,26 +2,23 @@
 -- Part 1
 
 -- Drop DB if exists
-DROP DATABASE IF EXISTS nba_db;
+--DROP DATABASE IF EXISTS nba_db;
 
 -- Create DB nba_db
-CREATE DATABASE nba_db;
+--CREATE DATABASE nba_db;
 
 -- Connect to nba_db
 \c nba_db;
 
 -- Create table here
-
+--create table players ( name text, age int, team char(3), games int, points int);
 
 -- Insert player here
-
+-- insert into players(name,age,team,games,points) values ('Anderson Varejao', 29, 'CLE', 25, 271);
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/mattgershowitz/code/wdi/turtle/homeworks/hw-u02-d06-psql_nba_style/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+COPY players(name, age, team, games, points) FROM '/Volumes/Monano/GA-SE-2019/sei/homeworks/hw-week04day04-sql/nba_season_2011-2012.csv' DELIMITER ',' CSV;
