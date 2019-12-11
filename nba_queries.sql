96ccd99a0605a11cd1c107cfeeb5fb3ef70c94a8
mashaelakh <56248434+mashaelakh@users.noreply.github.com>
2019-12-11 18:59:06 +0300

    commit

--- nba_queries.sql
+++ nba_queries.sql
@@ -4,11 +4,12 @@
 --------------------------------------------
 
 -- All columns for all players from the New York Knicks (NYK).
-
+SELECT * FROM players WHERE Team = 'NYK';
 
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
+SELECT * FROM players WHERE Team = 'IND' and Age < 26 ;
 
 
 
@@ -16,22 +17,24 @@
 -- scored.
 
 
+SELECT * FROM players ORDER BY points ASC;
+
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
-
+select name, points/games AS NEW from players ORDER BY NEW DESC LIMIT 20;
 
 
 -- The average age for all players.
 
-
+SELECT AVG(age) FROM players;
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
 
-
+SELECT AVG(age) from players where team ='OKC';
 
 -- The average age for all players who played more than 40 games.
-
+SELECT AVG(age) from players where games > 40;
 
 
 
@@ -43,10 +46,12 @@
 -- The team and total points scored from all players on that team (team points)
 -- ordered from most team points to least.
 
-
+SELECT team, SUM (Points) FROM players GROUP BY team;
 
 -- The age and the average points per game for that age, ordered from oldest to
 -- youngest for all ages.
+
+-- select age, AVG(points/game) from players GROUP BY age ORDER BY ASC;
 
 
 
--- nba_seed.sql
+++ nba_seed.sql
@@ -12,16 +12,23 @@ CREATE DATABASE nba_db;
 
 -- Create table here
 
+ CREATE TABLE players (
+ name text,
+ age integer,
+ team varchar(3),
+ games int,
+ points int);
+
 
 -- Insert player here
 
-
-----------------------------------
+INSERT INTO players ( name , age , team , games , points ) values ( 'Anderson Varejao' , 29 , 'CLE' , 25 , 271 );
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/mattgershowitz/code/wdi/turtle/homeworks/hw-u02-d06-psql_nba_style/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+COPY players
+  (name, age, team, games, points)
+FROM '/Users/mashael/Desktop/sei/homeworks/hw-week04day04-sql/nba_season_2011-2012.csv'
+    DELIMITER ',' CSV;
+    
