70804651495c5b4f906495358b8611341f5ace25
AlBattalSara <YOUR GITHUB EMAIL>
2019-12-12 08:59:53 +0300

    MySQLHW updated

--- nba_queries.sql
+++ nba_queries.sql
@@ -4,33 +4,34 @@
 --------------------------------------------
 
 -- All columns for all players from the New York Knicks (NYK).
-
+    select team from nba_players where team LIKE 'NYK=' ;
 
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
 
-
+nba_db=# select * from players where team ='IND' and age <=26;
 
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
-
+select * from nba_players order by points ASC;
 
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
-
+select  points,games  from nba_players order by points/game DESC limit 20;
 
 
 -- The average age for all players.
-
+select AVG(age) from nba_players;
 
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
 
-
-
+select AVG(age)
+ from nba_players where team ='OKC%';
 -- The average age for all players who played more than 40 games.
+select AVG FROm nba_players where game < 40; 
 
 
 
