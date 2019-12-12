12121fbeee843844250c7d085406d1cc8995158f
zanaalma <zanaalma1@gmail.com>
2019-12-12 09:05:30 +0300

    sql hw

--- nba_seed.sql
+++ nba_seed.sql
@@ -24,8 +24,12 @@ CREATE TABLE players ( name varchar,age integer, team varchar , games varchar, p
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
+\copy players(name,age,team,games,point) FROM '/Users/mashail/Desktop/sei/homework/hw-week04day04-sql/nba_season_2011-2012.csv'  WITH (FORMAT csv , HEADER false)
+-- \copy books(title, author, original_language,first_published) FROM 'data/books.csv' WITH (FORMAT csv, HEADER true)
 
+
+-- SELECT * FROM players;
 --COPY players
   -- (name, age, team, games, points)
-FROM '/Users/mashail/Desktop/sei/homework/hw-week04day04-sql/nba_season_2011-2012.csv'DELIMITER ',' CSV;
+-- FROM '/Users/mashail/Desktop/sei/homework/hw-week04day04-sql/nba_season_2011-2012.csv'DELIMITER ',' CSV;
  
