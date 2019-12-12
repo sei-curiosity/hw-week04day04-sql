05ec7283636761bfb4f2ab55bcc1c0a4c4c54be2
leenahabi <leenaalnahabi@gmail.com>
2019-12-12 07:00:34 +0300

    wk0404hw

--- nba_queries.sql
+++ nba_queries.sql
@@ -5,34 +5,34 @@
 
 -- All columns for all players from the New York Knicks (NYK).
 
-
+SELECT * FROM players WHERE team = 'NYK' ;
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
 
-
+SELECT * FROM players WHERE team = 'IND' AND age < 26 ;
 
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
 
-
+SELECT * FROM players ORDER BY points ASC ;  
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
 
-
+SELECT name , points/games AS pointpergame FROM players ORDER BY (points/games) DESC LIMIT 20;
 
 -- The average age for all players.
 
-
+SELECT AVG(age) FROM players ;
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
 
-
+SELECT AVG(age) FROM players WHERE team = 'OKC';
 
 -- The average age for all players who played more than 40 games.
 
-
+SELECT AVG(age) FROM players WHERE games < 40;
 
 
 --------------------------------------------
@@ -43,12 +43,13 @@
 -- The team and total points scored from all players on that team (team points)
 -- ordered from most team points to least.
 
-
+SELECT team , SUM(points) FROM players GROUP BY team;
 
 -- The age and the average points per game for that age, ordered from oldest to
 -- youngest for all ages.
 
-
+SELECT age, AVG(points) FROM players GROUP BY age ORDER BY age DESC;
 
 -- The team and the the number of players who score above 12 points per game on
 -- that team, ordered from the most number of players to the least number of players.
+SELECT team , COUNT(*) AS numofp FROM players WHERE (points/games)> 12 GROUP BY team ORDER BY numofp DESC;
--- nba_seed.sql
+++ nba_seed.sql
@@ -11,17 +11,24 @@ CREATE DATABASE nba_db;
 \c nba_db;
 
 -- Create table here
+CREATE TABLE players (
+name text,
+age int,
+team VARCHAR(15),
+games int,
+points int
+);
 
 
 -- Insert player here
-
+INSERT INTO players values ('Anderson Varejao', 29 , 'CLE' , 25 , 271 );
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/mattgershowitz/code/wdi/turtle/homeworks/hw-u02-d06-psql_nba_style/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+COPY players
+  (name, age, team, games, points)
+FROM '/Users/lina/Desktop/sei/homeworks/hw-week04day04-sql/nba_season_2011-2012.csv'
+    DELIMITER ',' CSV;
