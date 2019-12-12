cbbe2e0d63b988c9a1462180538f569362acd690
Abdulah68 <abdulahali6565@gmail.com>
2019-12-12 07:08:53 +0300

    solve hw-week04day04

--- nba_seed.sql
+++ nba_seed.sql
@@ -8,18 +8,28 @@ DROP DATABASE IF EXISTS nba_db;
 CREATE DATABASE nba_db;
 
 -- Connect to nba_db
-\c nba_db;
+ \c nba_db;
 
 -- Create table here
+CREATE TABLE players (
+ 
+  name text,
+  age INTEGER,
+  team text,
+  games INTEGER,
+  points INTEGER
 
+);
 
 -- Insert player here
+INSERT INTO players (name, age, team, games, points ) VALUES ('Anderson Varejao', 29, 'CLE', 25, 271);
 
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
+COPY players (name, age, team, games, points ) FROM 'C:/Users/alfares/Desktop/sei/homeworks/hw-week04day04-sql/nba_season_2011-2012.csv' DELIMITER ',' CSV;
 
 --COPY players
   --(name, age, team, games, points)
