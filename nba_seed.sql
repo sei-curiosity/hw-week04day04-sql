ebcd52a320ca39649624016176b5af508d1df97e
ahmedalhindi <alhindi.ahmed@gmail.com>
2019-12-12 09:03:26 +0300

    Completed but had an issue with the last bonus

--- nba_seed.sql
+++ nba_seed.sql
@@ -11,9 +11,17 @@ CREATE DATABASE nba_db;
 \c nba_db;
 
 -- Create table here
-
+CREATE TABLE players(
+  name TEXT,
+  age INT,
+  team TEXT,
+  game INT,
+  points INT
+);
 
 -- Insert player here
+INSERT INTO players
+VALUES ('Anderson Varejao',29,'CLE',25,271)
 
 
 ----------------------------------
@@ -21,7 +29,4 @@ CREATE DATABASE nba_db;
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/mattgershowitz/code/wdi/turtle/homeworks/hw-u02-d06-psql_nba_style/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+\COPY players (name, age, team, game, points) FROM '/Users/ahmedalhindi/sei/homeworks/week04/hw3/hw-week04day04-sql/nba_season_2011-2012.csv' DELIMITER ',' CSV;
