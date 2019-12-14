d4cc3708e685185dc167c7f308040e4dd59da366
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-14 20:30:35 +0300

    part 1 && part 2

--- nba_seed.sql
+++ nba_seed.sql
@@ -11,17 +11,21 @@ CREATE DATABASE nba_db;
 \c nba_db;
 
 -- Create table here
-
+create table players (
+  id serial primary key,
+  name varchar(255),
+  age int,
+  team varchar(255),
+  games int,
+  points int
+);
 
 -- Insert player here
-
+insert into players (name, age, team, games, points) values ('Anderson Varejao', 29, 'CLE', 25, 271);
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/mattgershowitz/code/wdi/turtle/homeworks/hw-u02-d06-psql_nba_style/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+\copy players (name, age, team, games, points) FROM '/home/alharbi/Desktop/sei/homeworks/week04/hw-week04day04-sql/nba_season_2011-2012.csv' DELIMITER ',' CSV;
