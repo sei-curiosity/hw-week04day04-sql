76c933e8f4f5974fc8b2ebff90be16d465c0071a
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-14 20:31:06 +0300

    part 3

--- nba_queries.sql
+++ nba_queries.sql
@@ -4,34 +4,34 @@
 --------------------------------------------
 
 -- All columns for all players from the New York Knicks (NYK).
-
+select * from players where team = 'NYK';
 
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
-
+select * from players where team = 'IND' and age < 26;
 
 
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
-
+select * from players order by points asc;
 
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
-
+select name, (points/games) as games_points from players order by points desc limit 20;
 
 
 -- The average age for all players.
-
+select name, avg(age) from players group by name;
 
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
-
+select name, avg(age) from players where team = 'OKC' group by name;
 
 
 -- The average age for all players who played more than 40 games.
-
+select name, avg(age) from players where games > 40 group by name;
 
 
 
